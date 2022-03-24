4ea56206905627b9ec3033de4f37f3563f547828
kgf-2 <102314990+kgf-2@users.noreply.github.com>
2022-03-24 22:09:31 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @Akshaymh17</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Akshay Chand</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @Akshaymh17    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
